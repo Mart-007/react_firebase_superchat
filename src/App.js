d1bf0338911829760890eb737c1eea570b0eb228
Reymart Bordo <reymart@ctoglobal.co>
2021-06-30 17:34:18 +0800

    initial commit / not yet working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+//https://www.youtube.com/watch?v=zQyrwxMPm88
+//https://github.com/fireship-io/react-firebase-chat
+import React from 'react';
 import './App.css';
+
+//import firebase sdk along with firestore and authentication
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+//import some hooks 
+import { useAuthState } from 'react-fibase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+//paste the initialize from your firebase here
+firebase.initializeApp({
+    apiKey: "AIzaSyBaMb4j9C_T9NVMKVJT5iYAgyeNXH56JE8",
+    authDomain: "bordsgc.firebaseapp.com",
+    projectId: "bordsgc",
+    storageBucket: "bordsgc.appspot.com",
+    messagingSenderId: "876876904361",
+    appId: "1:876876904361:web:f9f55f29d7547f39f6f6b8",
+    measurementId: "G-G5FEDPPSZK"
+})
+ //set authentication as global variables
+ const auth = firebase.auth();
+ const firestore = firebase.firestore();
+ const analytics = firebase.analytics();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is for testing </h1>
     </div>
   );
 }
